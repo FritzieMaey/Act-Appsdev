ac7f7d047a7c8228eacc6fe01e05d0bb12d2f5dc
Fritzie <fritziemaeson@gmail.com>
2026-02-18 10:12:17 +0800

    First Project

---
+++ main.dart
@@ -0,0 +1,16 @@
+class Store{
+  String? product;
+  double? price;
+  int? quality;
+}
+ 
+
+ void add(){
+  print("Enter choice: ");
+   print("Enter product name: ");
+    print("Enter price: ");
+     print("Enter quantity: ");
+     print("Product added successfully");
+ }
+
+ 
